025c954cdcb9acfa0c18c12b8eed71a069915890
Mikey J <mikeylashaun@icloud.com>
2021-05-18 19:49:30 -0700

    writing Psuedo code for the first page

---
+++ Assets/JS/javaScript.js
@@ -0,0 +1,7 @@
+// To make the screens switch we are going to have to push and pop elements and classes
+
+// First page I will need to make a h1, p, and button inside the main element
+// The h1 will have the title, append that first with its Unique tag
+// the p will go under and centered with its unique tag
+// the button under will say start with background yellow and black border, with slight border radius 
+// when the start button is pressed with eventlistener it will trigger a function to pop all of the elements off the page and it will trigger the next function with the first set off questions
